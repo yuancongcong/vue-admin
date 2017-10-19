5ca8365a222834b67bd58431ef7033450b9b87c1
袁聪聪 <772610229@qq.com>
2017-10-19 15:03:39 +0800

    fix

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -54,7 +54,7 @@ module.exports = {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         loader: 'url',
         query: {
-          limit: 1000,
+          limit: 10000,
           name: utils.assetsPath('img/[name].[hash:7].[ext]')
         }
       },
